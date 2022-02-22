0eaa162d623fdd2b38fc01a8d7ec350892b3a7ae
deveshrawat <devsrawt@gmail.com>
2022-02-22 12:51:01 +0530

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_RPC_URL)
+      },
+      network_id: '4',
+      skipDryRun: true,
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MAINNET_MNEMONIC, process.env.MAINNET_RPC_URL)
+      },
+      network_id: '1',
+      skipDryRun: true,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.6.6',
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
