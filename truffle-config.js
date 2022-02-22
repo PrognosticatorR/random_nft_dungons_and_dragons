a118f3872eb54cfb64826525b36408f5ad44b8af
deveshrawat <devsrawt@gmail.com>
2022-02-22 12:54:04 +0530

    update solc version

--- truffle-config.js
+++ truffle-config.js
@@ -1,32 +1,36 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
 
 module.exports = {
   networks: {
     rinkeby: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_RPC_URL)
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          process.env.RINKEBY_RPC_URL
+        );
       },
-      network_id: '4',
+      network_id: "4",
       skipDryRun: true,
     },
     mainnet: {
       provider: () => {
-        return new HDWalletProvider(process.env.MAINNET_MNEMONIC, process.env.MAINNET_RPC_URL)
+        return new HDWalletProvider(
+          process.env.MAINNET_MNEMONIC,
+          process.env.MAINNET_RPC_URL
+        );
       },
-      network_id: '1',
+      network_id: "1",
       skipDryRun: true,
     },
   },
   compilers: {
     solc: {
-      version: '0.6.6',
+      version: "0.8.12",
     },
   },
   api_keys: {
-    etherscan: process.env.ETHERSCAN_API_KEY
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
-  plugins: [
-    'truffle-plugin-verify'
-  ]
-}
+  plugins: ["truffle-plugin-verify"],
+};
